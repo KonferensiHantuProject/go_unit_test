43144c095f3749ff4b40b622e6679872f90d7949
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2024-02-18 19:43:02 +0700

    Adding Fail in Unit Test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,19 +1,27 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
+func TestHelloWorldNew(t *testing.T) {
 	result := HelloWorld("Eko")
 	if result != "Halo Eko" {
 		// Unit Test gagal
-		panic("Hasilnya gagal")
+		// t.Fail()
+		t.Error("Harusnya Halo Eko")
 	}
+	fmt.Println("Tes aja hehe")
 }
 
 func TestHelloWorldSecond(t *testing.T) {
 	result := HelloWorld("Kedua")
 	if result != "Halo Kedua" {
 		// Unit Test gagal
-		panic("Hasilnya gagal")
+		// t.FailNow()
+		t.Fatal("Hasilnya Harus Halo Kedua")
 	}
+
+	fmt.Println("Tes aja hehe second")
 }
